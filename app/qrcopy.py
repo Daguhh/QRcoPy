fd6bdd090595dce2fd2ee4e12d2f6c8aa15836bc
Daguhh <code.daguhh@zaclys.net>
2021-04-01 10:50:07 +0200

    add resizing + update requirements

--- app/qrcopy.py
+++ app/qrcopy.py
@@ -29,16 +29,20 @@ class Example(QWidget):
 
     def initUI(self):
 
+        self.dx = 0
+        self.dy = 0
+
         grid = QGridLayout()
 
         textqr = QLabel('Enter text to convert')
         self.textboxqr = QTextEdit()
+        self.textboxqr.setMaximumWidth(150)
         grid.addWidget(textqr, 1, 6)
         grid.addWidget(self.textboxqr, 2, 6)
 
-        pixmap = QPixmap(200,200)
+        self.pixmap = QPixmap(200,200)
         self.label = QLabel(self)
-        self.label.setPixmap(pixmap)
+        self.label.setPixmap(self.pixmap)
         grid.addWidget(self.label, 0, 0, 5, 5)
         #grid.addItem(pixmap,1,0,4,4)
 
@@ -47,17 +51,37 @@ class Example(QWidget):
         btn.clicked.connect(self.generate)
         grid.addWidget(btn,3,6)
 
+        self.generate()
+
         self.setLayout(grid)
         self.setGeometry(300, 300, 350, 200)
         self.setWindowTitle('Generate QRcodes')
+
+
         self.show()
 
     def generate(self):
+
         text = self.textboxqr.toPlainText()
         img = qrcode.make(text)
-        qim = ImageQt(img)
-        pixmap = QPixmap.fromImage(qim)
-        self.label.setPixmap(pixmap)
+        self.qim = ImageQt(img)
+        qim = self.qim.scaledToHeight(200)
+        self.pixmap = QPixmap.fromImage(qim)
+        import ipdb
+        #ipdb.set_trace()
+        self.label.setPixmap(self.pixmap)
+
+    def resizeEvent(self, e):
+
+        print(self.label.size())
+        self.dx += e.size().width() - e.oldSize().width()
+        self.dy += e.size().height() - e.oldSize().height()
+        #dx, dy = [x2 - x1 for x1, x2 in zip(e.size()., e.oldSize())]
+        print(f'dx : {self.dx}, dy : {self.dy}')
+
+        qim = self.qim.scaledToHeight(self.label.size().height()-10)
+        self.pixmap = QPixmap.fromImage(qim)
+        self.label.setPixmap(self.pixmap)
 
 if __name__ == '__main__':
 
